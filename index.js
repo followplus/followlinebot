de2641ddf999dfaa55299185cd38b3cd0ca779d2
followplus <32863690+followplus@users.noreply.github.com>
2017-10-17 16:27:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-var express = require('express');
-var app     = express();
+const express = require('express')
+const middleware = require('@line/bot-sdk').middleware
+
+const app = express()
+
+const config = {
+  channelAccessToken: 'p5lgfavV9OOvKaVfuST/I2hUoxUyghh02ErUY7d1eyrrLGNzNFmWMha2K1Xd/xv19Bjh1wTfpNFDuwqAtJ9dW25EjwjIGZM+ayNGqb6dU5pXnuN7S8aqr2yzpWYtlMnKCxkkeOmWVtpH0F7pu33ilQdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '17bd5614fefc02b11e78ad145173b22b'
+}
 
 app.set('port', (process.env.PORT || 5000));
 
-//For avoidong Heroku $PORT error
-app.get('/test', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
+app.post('/webhook', middleware(config), (req, res) => {
+  consoel.log(req.body.events);
+  res.json(req.body.events) // req.body will be webhook event object
 });
-app.post('/webhook', function(request, response) {
-    var result = 'App is running'
-    console.log(request);
-    response.send(result);
-});
+
 app.listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
