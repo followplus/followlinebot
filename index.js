33d0a175d918e85e63116bfac8c3ed58d38d309c
followplus <32863690+followplus@users.noreply.github.com>
2017-10-18 09:45:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const middleware = require('@line/bot-sdk').middleware
 const Client = require('@line/bot-sdk').Client;
 
 const app = express()
-/*
+
 const config = {
   channelAccessToken: 'p5lgfavV9OOvKaVfuST/I2hUoxUyghh02ErUY7d1eyrrLGNzNFmWMha2K1Xd/xv19Bjh1wTfpNFDuwqAtJ9dW25EjwjIGZM+ayNGqb6dU5pXnuN7S8aqr2yzpWYtlMnKCxkkeOmWVtpH0F7pu33ilQdB04t89/1O/w1cDnyilFU=',
   channelSecret: '17bd5614fefc02b11e78ad145173b22b'
 }
-*/
 
-const config = {
+
+const configDev = {
   channelAccessToken: '3NI2H6cTit07WzGMjeT+eVuT6XTCmJ887/h7MaGxZPywSwVTXDyqiDP5J7vYtQmzXD02341vXEGVnjzm6mM4x54asOM5Ebe9O/jtGMyIWwTVOaQIuz8Kb2DI+77XsoKvbqInRA8QWiPcVsgVlHgaSgdB04t89/1O/w1cDnyilFU=',
   channelSecret: 'ed4470bbaf9d7871bfbb1b2666fdd52f'
 }
 
-const client = new Client(config);
+const client = new Client(configDev);
 
 app.set('port', (process.env.PORT || 5000));
 
